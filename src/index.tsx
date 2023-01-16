ba08dee6e5281418348e884aef0c029c1a52126a
xoxexxx <fupi018@gmail.com>
2023-01-16 11:45:57 +0300

    install less-- add menu (navbar)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "./App";
-import './index.scss';
-
-
+import './index.less';
+import { Provider } from "react-redux";
+import { store } from "./services/store";
+import { BrowserRouter } from "react-router-dom";
 const root = (document.getElementById("root") as HTMLElement);
 const app = createRoot(root);
-app.render(<App  />);  
+app.render(
+    <Provider store={store} >
+        <BrowserRouter>
+            <App  />
+        </BrowserRouter>
+    </Provider>
+);  
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+export const Home = () => {
+
+    useEffect(() => {
+        document.title = `Creatiqa`
+    }, [])
+
+    return(
+        <div></div>
+    )
+}
