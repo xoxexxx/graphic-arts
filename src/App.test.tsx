ea86d51c392853505337d7d8bdb2fe53af86f898
xoxexxx <fupi018@gmail.com>
2023-01-18 18:38:08 +0300

    18:38

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import {App} from '../src/App'
-
-test('first render', () => {
-    render(<App  />)
-    const link =  screen.getByText(/CREATIQA/ig);
-    expect(link).toBeTruthy()
-})
--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  // moduleNameMapper: {
-  //   '^.+.(svg)$': 'jest-transform-stub',
-  // }
-};
